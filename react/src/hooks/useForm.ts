e7421e996275a106d6184cfac2e6315569714118
Wollzok <yurasikx22@gmail.com>
2025-04-28 09:40:35 +0300

    fixUseForm

--- react/src/hooks/useForm.ts
+++ react/src/hooks/useForm.ts
@@ -1,19 +1,15 @@
 import { useRef, useState } from 'react';
 
-interface FormProps {
-  [key: string]: string
-}
-
-interface SetValueForm {
+interface ISetValueForm {
   (key: string, value: string): void;
 }
 
-export const useForm = (form: FormProps): [FormProps, SetValueForm] => {
+export const useForm = (form: Record<string, string>): [Record<string, string>, ISetValueForm] => {
   const [, forceUpdate] = useState<NonNullable<unknown>>();
 
   const stateRef = useRef(form);
 
-  const setValue: SetValueForm = (key, value) => {
+  const setValue: ISetValueForm = (key, value) => {
     stateRef.current[key] = value;
     forceUpdate({});
   };
